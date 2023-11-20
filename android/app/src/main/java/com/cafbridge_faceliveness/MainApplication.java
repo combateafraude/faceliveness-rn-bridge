a3818b28d963a6b1bfb0a56c420e1e0718922251
lorena-zanferrari <lorena.zanferrari@msn.com>
2023-11-20 18:53:02 -0400

    added android bridge for faceliveness

--- android/app/src/main/java/com/cafbridge_faceliveness/MainApplication.java
+++ android/app/src/main/java/com/cafbridge_faceliveness/MainApplication.java
@@ -1,6 +1,7 @@
 package com.cafbridge_faceliveness;
 
 import android.app.Application;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
@@ -24,7 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+           packages.add(new CafPackage());
           return packages;
         }
 
